bb25faed6763dfeb1e86dc8c117798b999219e4c
LimYeonKyuu <lim000606@naver.com>
2024-02-27 16:56:10 +0900

    init

---
+++ src/main/java/com/example/springbootcrudtutorial/SpringBootCrudTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcrudtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootcrudtutorial/SpringBootCrudTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcrudtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
